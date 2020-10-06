f35418b29b2b9c1d7c154a12a66ffdfb1c0d7004
cryptobfund <cryptobfund@gmail.com>
2020-10-07 01:16:18 +0300

    step 3

--- tests/Feature/TaskStatusTest.php
+++ tests/Feature/TaskStatusTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\TaskStatus;
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -23,8 +24,8 @@ class TaskStatusTest extends TestCase
     {
         parent::setUp();
 
-    //    $this->taskStatus = factory(TaskStatus::class)->create();
-    //    $this->user = factory(User::class)->create();
+        $this->taskStatus = TaskStatus::factory()->create();
+        $this->user = User::factory()->create();
     }
 
     public function testWelcome()
@@ -33,17 +34,55 @@ class TaskStatusTest extends TestCase
         $response->assertOk();
     }
 
-    //public function testIndex()
-    //{
-    //    $response = $this->get(route('task_statuses.index'));
-    //    $response->assertOk();
-    //}
+    public function testIndex()
+    {
+        $response = $this->get(route('task_statuses.index'));
+        $response->assertOk();
+    }
 
-    //public function testCreate()
-    //{
-    //    $response = $this
-    //       ->actingAs($this->user)
-    //        ->get(route('task_statuses.create'));
-    //    $response->assertOk();
-    //}
+    public function testCreate()
+    {
+        $response = $this
+            ->actingAs($this->user)
+            ->get(route('task_statuses.create'));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = Factory::create()->text(30);
+        $response = $this
+            ->actingAs($this->user)
+            ->post(route('task_statuses.store', ['name' => $data]));
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseHas('task_statuses', ['name' => $data]);
+    }
+
+    public function testEdit()
+    {
+        $response = $this
+            ->actingAs($this->user)
+            ->get(route('task_statuses.edit', $this->taskStatus));
+        $response->assertOk();
+    }
+
+    public function testUpdate()
+    {
+        $data = Factory::create()->text(30);
+        $response = $this
+            ->actingAs($this->user)
+            ->patch(route('task_statuses.update', $this->taskStatus), ['name' => $data]);
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseMissing('task_statuses', ['name' => $this->taskStatus->name]);
+        $this->assertDatabaseHas('task_statuses', ['name' => $data]);
+    }
+
+    public function testDelete()
+    {
+        $response = $this
+            ->actingAs($this->user)
+            ->delete(route('task_statuses.destroy', $this->taskStatus));
+        $response->assertSessionHasNoErrors();
+        $this->assertDeleted('task_statuses', [$this->taskStatus]);
+    }
 }
